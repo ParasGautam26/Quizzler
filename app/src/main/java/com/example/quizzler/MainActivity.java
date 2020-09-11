4f8dc2c4198b9c6b4b0d56de51dbd54b8f388dac
Paras Gautam <48523128+ParasGautam26@users.noreply.github.com>
2020-09-11 17:20:45 +0530

    Adding ques to the view

--- app/src/main/java/com/example/quizzler/MainActivity.java
+++ app/src/main/java/com/example/quizzler/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.quizzler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button mTrueButton,mfalseButton;
-
+    Button mTrueButton,mFalseButton;
+    TextView mQuestionTextView;
+    int mIndex,mQuestion;
+    private TrueFalse[] mQuestionBank = new TrueFalse[]{
+            new TrueFalse(R.string.question_1,true),
+            new TrueFalse(R.string.question_2,true),
+            new TrueFalse(R.string.question_3,true),
+            new TrueFalse(R.string.question_4,true),
+            new TrueFalse(R.string.question_5,true),
+            new TrueFalse(R.string.question_6,false),
+            new TrueFalse(R.string.question_7,true),
+            new TrueFalse(R.string.question_8,false),
+            new TrueFalse(R.string.question_9,true),
+            new TrueFalse(R.string.question_10,true),
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +35,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTrueButton = findViewById(R.id.button);
-        mfalseButton = findViewById(R.id.button2);
+        mFalseButton = findViewById(R.id.button2);
+        mQuestionTextView = findViewById(R.id.textView);
 
+        mQuestion = mQuestionBank[mIndex].getQuestionID();
+        mQuestionTextView.setText(mQuestion);
+
+
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "True button pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "False button pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        TrueFalse example = new TrueFalse(R.string.question_1,true);
 
     }
 }

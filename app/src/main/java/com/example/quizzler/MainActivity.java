16ce39623ebc5aaa1526a07b64aca585e0762464
Paras Gautam <48523128+ParasGautam26@users.noreply.github.com>
2020-09-11 17:53:05 +0530

    Updating score and added methods

--- app/src/main/java/com/example/quizzler/MainActivity.java
+++ app/src/main/java/com/example/quizzler/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,9 +14,13 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    final int PROGRESS_BAR_INCREMENT = 10;
+
     Button mTrueButton,mFalseButton;
     TextView mQuestionTextView;
-    int mIndex,mQuestion;
+    int mIndex,mQuestion,mScore;
+    TextView mScoreTextView;
+    ProgressBar mProgressBar;
     private TrueFalse[] mQuestionBank = new TrueFalse[]{
             new TrueFalse(R.string.question_1,true),
             new TrueFalse(R.string.question_2,true),
@@ -37,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
         mTrueButton = findViewById(R.id.button);
         mFalseButton = findViewById(R.id.button2);
         mQuestionTextView = findViewById(R.id.textView);
+        mScoreTextView = findViewById(R.id.textView2);
+        mProgressBar = findViewById(R.id.progressBar);
 
         mQuestion = mQuestionBank[mIndex].getQuestionID();
         mQuestionTextView.setText(mQuestion);
@@ -45,18 +52,37 @@ public class MainActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "True button pressed", Toast.LENGTH_SHORT).show();
+               // Toast.makeText(MainActivity.this, "True button pressed", Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
+                UpdateQuestion();
             }
         });
 
         mFalseButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "False button pressed", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(MainActivity.this, "False button pressed", Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
+                UpdateQuestion();
             }
         });
+    }
+    private void UpdateQuestion(){
+        mIndex = (mIndex+1)%mQuestionBank.length;
+        mQuestion = mQuestionBank[mIndex].getQuestionID();
+        mQuestionTextView.setText(mQuestion);
+        mProgressBar.incrementProgressBy(PROGRESS_BAR_INCREMENT);
+    }
 
-        TrueFalse example = new TrueFalse(R.string.question_1,true);
-
+    private void checkAnswer(boolean userSelection){
+        boolean correctAnswer = mQuestionBank[mIndex].isAnswer();
+        if(correctAnswer == userSelection){
+            Toast.makeText(this,R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            mScore+=1;
+            mScoreTextView.setText("Score :"+mScore+"/"+mQuestionBank.length);
+        }
+        else{
+            Toast.makeText(this,R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+        }
     }
 }
